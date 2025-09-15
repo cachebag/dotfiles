4dc4f47afc6f68fbd5b7169659d3bf4b4a1b2e6b
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2025-09-15 13:40:41 -0400

    swaylock

---
+++ scripts/lock.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+WALLPAPER_DIR="$HOME/Pictures/lockscreen"
+CACHE_DIR="$HOME/.cache/lockscreen"
+
+mkdir -p "$CACHE_DIR"
+IMG=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+HASH=$(sha1sum "$IMG" | cut -d' ' -f1)
+BLURRED="$CACHE_DIR/${HASH}.png"
+
+if [[ ! -f "$BLURRED" ]]; then
+    convert "$IMG" -blur 0x8 -brightness-contrast -10x-10 "$BLURRED"
+fi
+
+swaylock \
+  --image "$BLURRED" \
+  --clock \
+  --indicator \
+  --indicator-radius 130 \
+  --indicator-thickness 10 \
+  --inside-color 282828cc \
+  --ring-color 83a598ff \
+  --key-hl-color b8bb26ff \
+  --bs-hl-color fb4934ff \
+  --line-color 00000000 \
+  --text-color ebdbb2ff \
+  --fade-in 0.3
+
